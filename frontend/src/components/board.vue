8a7aa6dc188a553684626ab72a233c6cf8d751af
Madinator <lvpjmd@gmail.com>
2022-05-13 03:42:18 +0600

    initial frontend

---
+++ frontend/src/components/board.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="board">
+        <div v-for="(section, index) in data" :key="index">
+            <label v-if="index < 9">{{section}}</label>
+            <div class="section">
+                <div v-for="item in section" :key="item" class="kumalak"></div>
+            </div>
+            <label v-if="index >= 9">{{section}}</label>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const data = ref([ 9, 9, 9, 9, 9, 9, 9, 9, 9,
+                   9, 9, 9, 9, 9, 9, 9, 9, 9])
+</script>
+
+<style>
+.board {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto auto auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+
+}
+
+.section {
+    display: grid;
+    grid-template-columns: auto auto;
+
+    width: 80px;
+    height: 200px;
+    background-color: aqua;
+    margin: 5px;
+    border: 3px solid black;
+    border-radius: 5px;
+}
+
+.kumalak {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: saddlebrown;
+    margin: auto;
+}
+
+</style>
